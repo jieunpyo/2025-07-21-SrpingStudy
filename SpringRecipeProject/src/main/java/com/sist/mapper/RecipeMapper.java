ca54030a6fccca6f9073e6602465f76d9fbcc826
jieunpyo <jieunhye1121@naver.com>
2025-12-02 16:34:45 +0900

    1111

---
+++ SpringRecipeProject/src/main/java/com/sist/mapper/RecipeMapper.java
@@ -0,0 +1,20 @@
+package com.sist.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Select;
+
+import com.sist.vo.RecipeVO;
+
+public interface RecipeMapper {
+   @Select("SELECT no,title,chef "
+		  +"FROM recipe "
+		  +"WHERE ${column} LIKE '%'||#{ss}||'%'")
+   public List<RecipeVO> recipeFindData(Map map);
+   
+   @Select("SELECT COUNT(*) "
+		   	  +"FROM recipe "
+		   	  +"WHERE ${column} LIKE '%'||#{ss}||'%'")
+   public int recipeFindCount(Map map);
+}
